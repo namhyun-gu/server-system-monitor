a8ce366da3c54eda8918b6e8407464b6d5f23468
namhyun-gu <mnhan0403@gmail.com>
2018-04-08 13:10:53 +0900

    feat: Add output port in server start log

--- server/src/main/java/server/system/monitor/server/Main.java
+++ server/src/main/java/server/system/monitor/server/Main.java
@@ -7,9 +7,10 @@ import server.system.monitor.listener.OnSocketEventListener;
 public class Main implements OnSocketEventListener {
     private static final int DEFAULT_PORT = 4040;
     private final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static int port = DEFAULT_PORT;
 
     public static void main(String[] args) {
-        int port = DEFAULT_PORT;
+        port = DEFAULT_PORT;
         if (args.length > 0) {
             port = Integer.parseInt(args[1]);
         }
@@ -19,7 +20,7 @@ public class Main implements OnSocketEventListener {
 
     @Override
     public void onStarted(int id) {
-        logger.info("Server started");
+        logger.info("Server started (port: " + port + ")");
     }
 
     @Override
